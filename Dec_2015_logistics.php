3851a072750dc5e8211dd279f6bd820d6ab592d0
Jeff Squyres <jsquyres@cisco.com>
2015-10-22 17:26:46 -0400

    Add location for Dec 2015 meeting.

---
+++ Dec_2015_logistics.php
@@ -0,0 +1,56 @@
+<?
+$topdir = ".";
+include_once("top-nav.php");
+
+$title = "December 7-10, 2015 Meeting Logistics";
+include_once("$topdir/include/header.php");
+?>
+
+<p align="right">
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12673.262443392721!2d-121.93341717646358!3d37.4296482985981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x29434aa4a14ef89e!2sCisco+SJ-McCarthy+Ranch+3!5e0!3m2!1sen!2sus!4v1445549128141" width="600" height="450" frameborder="0" style="border:0" allowfullscreen align=right></iframe>
+
+<ul>
+<li> <a href="#registration">Meeting registration</a></li>
+<li> <a href="#location">Meeting location</a></li>
+<li> <a href="#cost">Meeting cost</a></li>
+<li> <a href="#hotel-block">Hotel room block</a></li>
+</ul>
+
+<div align="center"><hr width=50%></div>
+
+<h3><a name=registration>Meeting Registration</a></h3>
+
+TBD.
+
+</p>
+
+<div align="center"><hr width=50%></div>
+
+<h3><a name=location>Meeting Location</a></h3>
+
+<p>The meeting will take place at:
+
+<li> Cisco building MR-3 (McCarthy Ranch), 155 North McCarthy Blvd.
+Milpitas, CA, USA.  Great Dane conference room.</li>
+</ul>
+
+<div align="center"><hr width=50%></div>
+
+<h3><a name=cost>Meeting Cost</a></h3>
+
+TBD.
+
+</p>
+
+
+<div align="center"><hr width=50%></div>
+
+<h3><a name=hotel-block>Hotel Room Block</a></h3>
+
+TBD.
+
+</p>
+
+<?
+include_once("$topdir/include/footer.php");
