290842937f1af8e89f03ddc13d547b9119b6c40a
schulz6 <schulz6@LOFT-e0accb735c12.local>
2016-02-09 22:44:25 -0800

    start of agenda page

---
+++ Mar_2016_logistics.php
@@ -0,0 +1,56 @@
+<?
+$topdir = ".";
+include_once("top-nav.php");
+
+$title = "February 29th - March 3rd, 2016 Meeting Logistics";
+include_once("$topdir/include/header.php");
+
+?>
+
+<h3><a name=registration>Meeting Registration</a></h3>
+
+
+<p>Advanced registration is required for this meeting - Registration will be available soon.</p>
+
+<!--
+<p><a
+href="https://www.eventbrite.com/e/mpi-forum-tickets-16703722283">Meeting Registration</a></p>
+-->
+
+
+<p>The registration covers snacks at the meeting on Monday, Tuesday
+Wednesday, and Thursday lunch on Tuesday and Wednesday, and the meeting logistics.  </p>
+
+
+<p>Advanced registration is required for this meeting so that Microsoft
+can process their visitor processing procedure.</p>
+
+<div align=center><hr width=50%></div>
+
+<h3><a name=location>Meeting Location</a></h3>
+
+<p>The meeting will take
+place at the Microsoft facility in the Aon Center, at <a href="<?
+print($map1_link); ?>">200 E. Randolph Drive, Suite 200, Chicago, IL
+60601, USA</a>.</p>
+
+<p>The Chicago Transit Authority <a
+href="http://www.transitchicago.com/riding_cta/systemguide/blueline.aspx">Blue
+Line train</a> goes from the O'Hare airport (ORD) to stops just a few
+blocks from the Microsoft facility.</p>
+
+<p>
+
+<div align=center><hr width=50%></div>
+
+<h3><a name=cost>Meeting Cost</a></h3>
+
+<p>$150+fees per person to cover meeting logistics
+costs, snacks on Monday, Tuesday, Wednesday, and Thursday, and
+lunch on Tuesday and Wednesday.
+&nbsp; </p>
+
+<div align=center><hr width=50%></div>
+
+<?
+include_once("$topdir/include/footer.php");
