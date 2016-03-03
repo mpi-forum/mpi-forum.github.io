c2866038c67134a637f675c0011eb31016a314dd
Jeff Squyres <jsquyres@cisco.com>
2016-03-03 13:09:35 -0600

    Add Fortran slides

--- secretary/2016/03/slides.php
+++ secretary/2016/03/slides.php
@@ -11,6 +11,7 @@ $slides[] = "2016-03-02-ULFM-in-Fenix-plenary";
 $slides[] = "2016-03-02-Catastrophic-Errors";
 $slides[] = "2016-03-02-issues-1-3-reading";
 $slides[] = "Pitch - Allocating Receive and Freeing Send";
+$slides[] = "fortran-datatypes-in-mpih";
 
 show_slides($slide_dir, $slides);
 
