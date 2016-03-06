6925613c956a9d67c7d3927ef55c1092971b6194
Martin Schulz <schulzm@llnl.gov>
2016-03-05 21:22:19 -0800

    added June logistics

---
+++ Jun_2016_logistics.php
@@ -0,0 +1,48 @@
+<?
+$topdir = ".";
+include_once("top-nav.php");
+
+$title = "June 6th - 9th, 2016 Meeting Logistics";
+include_once("$topdir/include/header.php");
+
+?>
+
+<h3><a name=registration>Meeting Registration</a></h3>
+
+
+<p>Advanced registration is required for this meeting - a link to the registration page will be available closer to the event.</p>
+
+<!--
+<p><a
+href="">Meeting Registration</a></p>
+-->
+
+<p>The registration covers snacks at the meeting on Monday, Tuesday
+Wednesday, and Thursday lunch on Tuesday and Wednesday, and the meeting logistics.  </p>
+
+
+<p>Advanced registration is required for this meeting so that Microsoft
+can process their visitor processing procedure.</p>
+
+<div align=center><hr width=50%></div>
+
+<h3><a name=location>Meeting Location</a></h3>
+
+<p>The meeting will take
+place at the Microsoft facility in downtown Bellevue, WA (<a HREF="https://www.google.com/maps/place/788+110th+Ave+NE,+Bellevue,+WA+98004/@47.616604,-122.1948417,17z/data=!3m1!4b1!4m2!3m1!1s0x54906c8a2a9e11c1:0x89a6ae4a28867a8e">788 110th Ave NE, Bellevue, WA 98004, United States</a>). Hotels downtown Bellevue are in walking distance. If you want/have to stay further away, it is suggested that you car pool, since parking is limited. If needed, though, there is parking available at Microsoft in a general garage. The receptionist, who is available from 8am to 5pm only, can validate the parking ticket.</p>
+
+<p>
+
+<div align=center><hr width=50%></div>
+
+<h3><a name=cost>Meeting Cost</a></h3>
+
+<p>$150+fees per person to cover meeting logistics
+costs, snacks on Monday, Tuesday, Wednesday, and Thursday, and
+lunch on Tuesday and Wednesday.
+&nbsp; </p>
+
+<div align=center><hr width=50%></div>
+
+<?
+include_once("$topdir/include/footer.php");
