cdfccb525d76bcc6a20f585db44245be80c41c69
Jeff Squyres <jsquyres@cisco.com>
2016-06-09 17:06:09 -0400

    More slides for 2016-06 Bellevue, WA, USA Forum meeting

--- secretary/2016/06/slides.php
+++ secretary/2016/06/slides.php
@@ -11,6 +11,7 @@ $slides[] = "sessions-wg-2016-06-07";
 $slides[] = "2016-06-08-Catastrophic-Errors";
 $slides[] = "2016-06-08-issues-1-3-reading";
 $slides[] = "2016-06-09 Error Handler Plenary";
+$slides[] = "2016-06-forum-general";
 
 show_slides($slide_dir, $slides);
 
