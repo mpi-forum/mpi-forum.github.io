c926db0d11d67b540bdc6ccb8718d2c581661758
Wesley Bland <wesley.bland@intel.com>
2017-09-07 10:15:43 -0500

    Clean up last php files

--- top-nav-3.0.php
+++
@@ -1,7 +0,0 @@
-<?
-include_once("$topdir/top-nav.php");
-include_once("$topdir/include/nav.php");
-
-$top_navlist[] = new TopNav("MPI 3.0", "$topdir/MPI_3.0_main_page.php");
-
-$navlist[] = new Nav("ABI WG", "mpi3.0_abi.php");
--- top-nav.php
+++
@@ -1,5 +0,0 @@
-<?
-include_once("$topdir/include/top-nav.php");
-
-$top_navlist[] = new TopNav("Meetings home", "$topdir/");
-
