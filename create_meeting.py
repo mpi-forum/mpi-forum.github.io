9bed2a978964b2d3fd3a87906dfb7dda98f982dd
Wesley Bland <wesley.bland@intel.com>
2019-07-11 15:47:46 -0500

    Improve the meeting creation script some more

--- create_meeting.py
+++ create_meeting.py
@@ -1,100 +1,106 @@
 #!/usr/bin/python
 
 import os;
-from datetime import datetime, date, time;
+from datetime import timedelta, datetime, date, time;
 
 #### RUN FROM TOP OF REPO ####
 
 # Get meeting information
-month = raw_input("2 digit month: ");
-year = raw_input("4 digit year: ");
-start_day = raw_input("Starting Day: ");
-end_day = raw_input("Ending Day: ");
+start_date = raw_input("Start date (YYYY-MM-DD): ")
+end_date = raw_input("Enddate (YYYY-MM-DD): ")
 location = raw_input("Location: ");
 
-start = date(int(year), int(month), int(start_day))
-end = date(int(year), int(month), int(end_day))
+start = datetime.strptime(start_date, "%Y-%m-%d")
+end = datetime.strptime(end_date, "%Y-%m-%d")
+
+start_day  = start.strftime("%d")
+start_month = start.strftime("%m")
+start_year = start.strftime("%Y")
+end_day  = end.strftime("%d")
 
 # Create the directories
-if not os.path.exists('meetings/' + year + '/' + month):
-    os.makedirs('meetings/' + year + '/' + month);
+if not os.path.exists('meetings/' + start_year + '/' + start_month):
+    os.makedirs('meetings/' + start_year + '/' + start_month);
 
-if not os.path.exists('_data/meetings/' + year + '/' + month):
-    os.makedirs('_data/meetings/' + year + '/' + month);
+if not os.path.exists('_data/meetings/' + start_year + '/' + start_month):
+    os.makedirs('_data/meetings/' + start_year + '/' + start_month);
 
 # Create the markdown files
-file = open('meetings/' + year + '/' + month + '/agenda.md', 'w');
+file = open('meetings/' + start_year + '/' + start_month + '/agenda.md', 'w');
 print >> file, "---";
 print >> file, "layout: agenda2";
 print >> file, "date: " + start.strftime("%B %d, %Y - ") + end.strftime("%B %d, %Y")
-print >> file, "permalink: meetings/" + year + "/" + month + "/agenda";
-print >> file, 'year: "' + year + '"';
-print >> file, 'month: "' + month + '"';
+print >> file, "permalink: meetings/" + start_year + "/" + start_month + "/agenda";
+print >> file, 'year: "' + start_year + '"';
+print >> file, 'month: "' + start_month + '"';
 print >> file, 'webex: ""';
 print >> file, "---\n";
 file.close();
 
-file = open('meetings/' + year + '/' + month + '/attendance.md', 'w');
+file = open('meetings/' + start_year + '/' + start_month + '/attendance.md', 'w');
 print >> file, "---";
 print >> file, "layout: attendance";
 print >> file, "date: " + start.strftime("%B %d, %Y - ") + end.strftime("%B %d, %Y")
-print >> file, "permalink: meetings/" + year + "/" + month + "/attendance";
-print >> file, 'year: "' + year + '"';
-print >> file, 'month: "' + month + '"';
+print >> file, "permalink: meetings/" + start_year + "/" + start_month + "/attendance";
+print >> file, 'year: "' + start_year + '"';
+print >> file, 'month: "' + start_month + '"';
 print >> file, "---\n";
 file.close();
 
-file = open('meetings/' + year + '/' + month + '/logistics.md', 'w');
+file = open('meetings/' + start_year + '/' + start_month + '/logistics.md', 'w');
 print >> file, "---";
 print >> file, "layout: logistics";
 print >> file, "date: " + start.strftime("%B %d, %Y - ") + end.strftime("%B %d, %Y")
-print >> file, "permalink: meetings/" + year + "/" + month + "/logistics";
-print >> file, 'year: "' + year + '"';
-print >> file, 'month: "' + month + '"';
+print >> file, "permalink: meetings/" + start_year + "/" + start_month + "/logistics";
+print >> file, 'year: "' + start_year + '"';
+print >> file, 'month: "' + start_month + '"';
 print >> file, "---\n";
 file.close();
 
-file = open('meetings/' + year + '/' + month + '/notes.md', 'w');
+file = open('meetings/' + start_year + '/' + start_month + '/notes.md', 'w');
 print >> file, "---";
 print >> file, "layout: notes";
 print >> file, "date: " + start.strftime("%B %d, %Y - ") + end.strftime("%B %d, %Y")
-print >> file, "permalink: meetings/" + year + "/" + month + "/notes";
+print >> file, "permalink: meetings/" + start_year + "/" + start_month + "/notes";
 print >> file, "title: " + start.strftime("%B %Y Meeting Notes");
 print >> file, "---\n";
 file.close();
 
-file = open('meetings/' + year + '/' + month + '/votes.md', 'w');
+file = open('meetings/' + start_year + '/' + start_month + '/votes.md', 'w');
 print >> file, "---";
 print >> file, "layout: votes";
 print >> file, "date: " + start.strftime("%B %d, %Y - ") + end.strftime("%B %d, %Y")
-print >> file, 'year: "' + year + '"';
-print >> file, 'month: "' + month + '"';
+print >> file, 'year: "' + start_year + '"';
+print >> file, 'month: "' + start_month + '"';
 print >> file, 'rules: 3152013';
-print >> file, "permalink: meetings/" + year + "/" + month + "/votes";
-print >> file, 'registered: ';
-print >> file, 'ooe: ';
-print >> file, 'imove: ';
+print >> file, "permalink: meetings/" + start_year + "/" + start_month + "/votes";
+print >> file, 'registered:';
+print >> file, 'ooe:';
+print >> file, 'imove:';
 print >> file, "---\n";
 file.close();
 
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days + 1)):
+        yield start_date + timedelta(n)
+
 # Create the data files
-file = open('_data/meetings/' + year + '/' + month + '/agenda.yml', 'w');
-print >> file, 'schedule: \n';
-for day in range(int(start_day), int(end_day)+1):
-    tmp_day = date(int(year), int(month), day)
-    print >> file, '    - day: ' + tmp_day.strftime("%A, %B %d") + '\n'
+file = open('_data/meetings/' + start_year + '/' + start_month + '/agenda.yml', 'w');
+print >> file, 'schedule:\n';
+for day in daterange(start, end):
+    print >> file, '    - day: ' + day.strftime("%A, %B %d") + '\n'
     print >> file, '    - start: 12:00am'
     print >> file, '    - end: 11:59pm'
     print >> file, '    - title: "Placeholder Agenda Item"\n'
     print >> file, '    - endday: done\n\n'
 
-print >> file, 'procedure-votes: \n';
+print >> file, 'procedure-votes:\n';
 print >> file, '    - number: 1234'
 print >> file, '    - description: "Placeholder Procedure Vote"\n'
-print >> file, 'errata-votes: \n';
-print >> file, 'no-no-votes: \n';
-print >> file, 'first-votes: \n';
-print >> file, 'second-votes: \n';
+print >> file, 'errata-votes:\n';
+print >> file, 'no-no-votes:\n';
+print >> file, 'first-votes:\n';
+print >> file, 'second-votes:\n';
 print >> file, 'plenaries:';
 print >> file, '    - type: Welcome'
 print >> file, '    - title: Introductions'
@@ -111,15 +117,15 @@ print >> file, '    - presenter: Placeholder Presenter'
 print >> file, '    - done: 0\n'
 file.close();
 
-file = open('_data/meetings/' + year + '/' + month + '/attendance.csv', 'w');
+file = open('_data/meetings/' + start_year + '/' + start_month + '/attendance.csv', 'w');
 print >> file, 'name,org,attend';
 file.close();
 
-file = open('_data/meetings/' + year + '/' + month + '/ballot.csv', 'w');
+file = open('_data/meetings/' + start_year + '/' + start_month + '/ballot.csv', 'w');
 print >> file, 'org,"Vote 1"';
 file.close();
 
-file = open('_data/meetings/' + year + '/' + month + '/votes.csv', 'w');
+file = open('_data/meetings/' + start_year + '/' + start_month + '/votes.csv', 'w');
 print >> file, 'topic,type,yes,no,abstain,missed';
 file.close();
 
