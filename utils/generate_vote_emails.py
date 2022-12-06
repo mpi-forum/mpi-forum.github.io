a26517f8bece7384d9e9d22ea29643d8dfa2f9c8
Wes Bland <work@wesbland.com>
2022-12-06 11:00:00 -0600

    Update vote script

--- utils/generate_vote_emails.py
+++ utils/generate_vote_emails.py
@@ -138,7 +138,7 @@ def main():
     curr_attendance_file   = "/Users/wbland/mpi/mpi-forum.github.io/_data/meetings/2022/12/attendance.csv"
     curr_registration_file = "/Users/wbland/mpi/meeting-details/2022-12-dec/2022-12-05-registration.csv"
     # Make sure to use a pre-filled link here so it gets email out correctly
-    registration_link = "https://docs.google.com/forms/d/e/1FAIpQLSeQvzqlrFShf8NNcmMYh0ZibacvRXvm7HvS71M2DIQrDaEP4w/viewform?usp=pp_url&entry.1569039753={name}&entry.1678977275={org}&entry.1823932772={id}"
+    registration_link = "https://docs.google.com/forms/d/e/1FAIpQLSeLVUl93msqKngVD5yk3_HOIpO7zBCys7T8gu7CDbTQUKbQ0A/viewform?usp=pp_url&entry.1569039753={name}&entry.1678977275={org}&entry.1823932772={id}"
 
     prev_ooe = 35
 
@@ -260,7 +260,7 @@ def main():
             message_text = """\
 Hi {name},
 
-Voting is now open for the December 2022 Plenary Day 2 meeting. You may vote at this
+Voting is now open for the December 2022 Plenary Day 3 meeting. You may vote at this
 link:
 <br><br>
 {link}
@@ -274,7 +274,7 @@ order to vote. If no one from your organization attended any portion of the
 meeting up to the point where first voting block opened, your organization's
 vote will not be counted.
 <br><br>
-Voting will be open until 10:35pm US Central time on December 6th, 2022.
+Voting will be open until 10:35pm US Central time on December 7th, 2022.
 <br><br>
 Thanks,
 <br>
@@ -282,7 +282,7 @@ Wes Bland (MPI Forum Secretary)\
                             """.format(name=name, link=text_link)
 
             message = service.create_message(from_addr='"MPI Forum Mailer Bot" <mpiforumbot@gmail.com>',
-                    to_addr=email, msg=message_text, subject='December 2022 MPI Forum Plenary Day 2 Voting Link')
+                    to_addr=email, msg=message_text, subject='December 2022 MPI Forum Plenary Day 3 Voting Link')
             #message_id = service.send_message(message=message)
 
 if __name__ == '__main__':
