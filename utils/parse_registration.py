cc593aa74cd93405196e931441efc5a2bd45673a
Wes Bland <work@wesbland.com>
2023-02-13 14:37:21 -0600

    Add UUID to registration

--- utils/parse_registration.py
+++ utils/parse_registration.py
@@ -2,19 +2,7 @@
 
 import csv
 import sys
-
-# For this function to work, the attendance list needs to include the exact fields as described
-# below. Otherwise this function won't be able to confirm that the ballot came from the correct
-# person (or that the person even registered).
-def confirm_id(id, name, org, attendance_list):
-    #print("MATCH ID for " + name + ", " + org);
-    for entry in iter(attendance_list):
-        #print(entry);
-        if (id == entry['UUID'] and
-                name == entry['What is your full name?'] and
-                org == entry['What organization will you be representing?']):
-            return 1;
-    return 0;
+import uuid
 
 def main():
     registration_file=sys.argv[1]; # File with registrations from Google Form
@@ -27,16 +15,19 @@ def main():
 
     registration_list = list(csv.DictReader(open(registration_file)));
 
-    writer = csv.DictWriter(open('attendance.csv', 'w', newline=''), ['name','org','attend'], quoting = csv.QUOTE_ALL);
-    writer.writeheader();
+    attendanceWriter = csv.DictWriter(open('attendance.csv', 'w', newline=''), ['name','org','attend'], quoting = csv.QUOTE_ALL);
+    attendanceWriter.writeheader();
+    registrationWriter = csv.DictWriter(open('registration.csv', 'w', newline=''), ['name','org','email','uuid'], quoting = csv.QUOTE_ALL);
+    registrationWriter.writeheader();
 
-    print("Writing attendance.csv...");
+    print("Writing attendance.csv and registration.csv...");
 
     names = {};
 
     for registration in iter(registration_list):
         name = registration['What is your full name?'];
         org = registration['What organization will you be representing?'];
+        email = registration['Email Address'];
 
         if name in names:
             print("Omitting duplicate name: " + name);
@@ -44,7 +35,8 @@ def main():
         else:
             names[name] = 1;
 
-        writer.writerow({'name': name, 'org': org, 'attend': '1'});
+        attendanceWriter.writerow({'name': name, 'org': org, 'attend': '1'});
+        registrationWriter.writerow({'name': name, 'org': org, 'email': email, 'uuid': uuid.uuid1()});
 
     print("\n=====\n");
 
