5d974930142217487d56a0114b22bcb149f30979
Wes Bland <work@wesbland.com>
2025-09-23 14:21:18 -0500

    Update attendance

--- utils/parse_eventbrite_registration.py
+++ utils/parse_eventbrite_registration.py
@@ -20,6 +20,7 @@ def main():
             "LLNL": "Lawrence Livermore National Laboratory",
             "Intel": "Intel Corporation",
             "TACC Austin": "The Texas Advanced Computing Center",
+            "TACC": "The Texas Advanced Computing Center",
             "Paratools SAS": "ParaTools SAS",
             "High Performance Computing Center Stuttgart": "HLRS, University of Stuttgart",
             "Sandia National Laboratories": "Sandia National Laboratory",
